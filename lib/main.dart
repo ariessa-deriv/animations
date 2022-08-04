4a2a790b65a4109140ec41b41bdedcb83aa65803
ariessa-deriv <>
2022-08-04 14:26:05 +0800

    Example of staggered animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:animations/multiple_animations.dart';
 import 'package:animations/simple_scale_animation.dart';
+import 'package:animations/staggered_animations.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MultipleAnimations(),
+      home: StaggeredAnimations(),
     );
   }
 }
---
+++ lib/staggered_animations.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class StaggeredAnimations extends StatefulWidget {
+  const StaggeredAnimations({Key? key}) : super(key: key);
+
+  @override
+  State<StaggeredAnimations> createState() => _StaggeredAnimationsState();
+}
+
+class _StaggeredAnimationsState extends State<StaggeredAnimations>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation _scaleAnimation;
+  late Animation _rotateAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    _scaleAnimation = CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(0.0, 0.5, curve: Curves.easeOutCubic));
+    _rotateAnimation = Tween<double>(begin: 0.0, end: 2 * 3.14159265).animate(
+        CurvedAnimation(
+            parent: _animationController, curve: Interval(0.0, 0.5)));
+
+    _animationController.addListener(() {
+      setState(() {});
+    });
+    _animationController.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+        angle: _rotateAnimation.value,
+        child: Transform.scale(
+            scale: _scaleAnimation.value,
+            child: Container(color: Colors.deepOrange, width: 50, height: 50)));
+  }
+}
